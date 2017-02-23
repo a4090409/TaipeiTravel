66764c40c01788b49e6825369cfd33c5f450be0f
buck chin <buck.chin@eitc.com.tw>
2017-02-23 16:53:35 +0800

    ＃修正變數

--- TaipeiTravel/ViewController.swift
+++ TaipeiTravel/ViewController.swift
@@ -14,39 +14,40 @@ import FirebaseDatabase
 
 class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
     
-    var urlString:String!
-    var dataArray = [AnyObject]()
-    var travelInfo:TravalInfo!
+        @IBOutlet var imageView:UIImageView!
+        var urlString:String!
+        var dataArray = [AnyObject]()
+        var travelInfo:TravalInfo!
+        var randomNum:UInt32!
+        var usedNum = [UInt32]()
+        var imgsArr:[String] = []
+        var busy:UIActivityIndicatorView!
+        var ref:FIRDatabaseReference!
+        var nowday:String!
+        var dataArrayTaipei:[JSON]!
+        let uuid = UIDevice.current.identifierForVendor?.uuidString
     
-    var randomNum:UInt32!
-    var usedNum = [UInt32]()
-    var imgsArr:[String] = []
-    var busy:UIActivityIndicatorView!
-    @IBOutlet var imageView:UIImageView!
+        //過場動畫
+        let customPresentAnimationController = CustomPresentAnimationController()
     
-    var ref:FIRDatabaseReference!
-    let uuid = UIDevice.current.identifierForVendor?.uuidString
-    
-    var nowday:String!
-    
-    var dataArrayTaipei:[JSON]!
-    let customPresentAnimationController = CustomPresentAnimationController()
     override func viewDidLoad() {
         
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        
+        //初始化物件
+        ref = FIRDatabase.database().reference()
+        travelInfo = TravalInfo()
+
+        //创建一个日期格式器
         let now = NSDate()
-        // 创建一个日期格式器
         let dformatter = DateFormatter()
         dformatter.dateFormat = "yyyyMMdd"
         nowday = dformatter.string(from: (now as Date))
         
-        ref = FIRDatabase.database().reference()
-
-        //初始化物件
-        travelInfo = TravalInfo()
-        
         //呼叫DataTaipei Api
         urlString = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=36847f3f-deff-4183-a5bb-800737591de5"
-
+        
+        //讀取
         let centerx = self.view.frame.width/2
         let centery = self.view.frame.height/2
 
@@ -54,19 +55,17 @@ class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
             self.busy = UIActivityIndicatorView(activityIndicatorStyle: .gray)
             self.busy.center = CGPoint(x:  centerx  , y: centery)
             self.view.addSubview(self.busy)
-            
             self.busy.hidesWhenStopped = true
             self.busy.startAnimating()
         }
 
-        // ref.child("321").childByAutoId().setValue("3345678")
-
         
+        //Firebase事件
+        // ref.child("321").childByAutoId().setValue("3345678")
         ref.observeSingleEvent(of: .value, with: {(snapshot) in
-           
             let existsUser = snapshot.childSnapshot(forPath: "\(self.uuid!)").exists()
             
-            //已存在User檢查當日是否更新
+            //已存在的uuid檢查當日是否更新
             if(existsUser)
             {
                 if let NowDate = snapshot.childSnapshot(forPath: "\(self.uuid!)/nowday").value
@@ -74,9 +73,9 @@ class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
                     //當日未更新
                     if (self.nowday! != NowDate as! String)
                     {
-                        //先清除使用過的推薦景點
-                    self.ref.child(self.uuid!).child("usedNum").setValue([])
-                        self.alamofireStar(SetTravalInfo: true)
+                        //清除使用過的推薦景點
+                     self.ref.child(self.uuid!).child("usedNum").setValue([])
+                         self.alamofireStar(SetTravalInfo: true)
 
                     }
                     //使用已更新過內容設定travaInfo
@@ -84,19 +83,14 @@ class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
                     {
                         self.alamofireStar(SetTravalInfo: false)
                         self.usedNum = snapshot.childSnapshot(forPath: "\(self.uuid!)/usedNum").value as! [UInt32]
-                        //self.AlamofireStar()
-
-                        //print("now = (SetTravalInfoBysnapshot)")
                         self.setTravalInfoBysnapshot(Snapshot: snapshot)
                     }
                 }
             }
             else
             {
-                //print("now = (AlamofireStar)")
-                //開始讀取api資料
+                //讀取api資料
                 self.alamofireStar(SetTravalInfo: true)
-                               //資料下載完成後
             }
             /*for rest in snapshot.children.allObjects as! [FIRDataSnapshot] {
                  //print(rest.key)
@@ -108,9 +102,7 @@ class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
            }*/
          })
         
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         super.viewDidLoad()
-        
     }
     
     
@@ -126,7 +118,7 @@ class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
         self.travelInfo.MEMO_TIME  = dataArrayTaipei[Int(self.randomNum)]["MEMO_TIME"].stringValue
         self.travelInfo.trafficinfo  = dataArrayTaipei[Int(self.randomNum)]["info"].stringValue
     }
-    
+    //Firebase回傳設定值
     func setTravalInfoBysnapshot(Snapshot snapshot:FIRDataSnapshot)
     {
         self.travelInfo.stitle = snapshot.childSnapshot(forPath: "\(self.uuid!)/stitle").value as! String
@@ -139,25 +131,27 @@ class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
         self.travelInfo.trafficinfo = snapshot.childSnapshot(forPath: "\(self.uuid!)/trafficinfo").value as! String
         self.ref.child(self.uuid!).child("nowday").setValue(self.nowday)
     }
+    //更新Firebase
     func  updateFireBase()
     {
-        self.ref.child(self.uuid!).child("stitle").setValue(self.travelInfo.stitle)
-        self.ref.child(self.uuid!).child("cate").setValue(self.travelInfo.cate)
-        self.ref.child(self.uuid!).child("address").setValue(self.travelInfo.address)
-        self.ref.child(self.uuid!).child("imgfile").setValue(self.travelInfo.imgfile)
-        self.ref.child(self.uuid!).child("MRT").setValue(self.travelInfo.MRT)
-        self.ref.child(self.uuid!).child("xbody").setValue(self.travelInfo.xbody)
-        self.ref.child(self.uuid!).child("MEMO_TIME").setValue(self.travelInfo.MEMO_TIME)
-        self.ref.child(self.uuid!).child("trafficinfo").setValue(self.travelInfo.trafficinfo)
-        self.ref.child(self.uuid!).child("nowday").setValue(self.nowday)
-    self.ref.child(self.uuid!).child("usedNum").setValue( self.usedNum)
+      self.ref.child(self.uuid!).child("stitle").setValue(self.travelInfo.stitle)
+      self.ref.child(self.uuid!).child("cate").setValue(self.travelInfo.cate)
+      self.ref.child(self.uuid!).child("address").setValue(self.travelInfo.address)
+      self.ref.child(self.uuid!).child("imgfile").setValue(self.travelInfo.imgfile)
+      self.ref.child(self.uuid!).child("MRT").setValue(self.travelInfo.MRT)
+      self.ref.child(self.uuid!).child("xbody").setValue(self.travelInfo.xbody)
+      self.ref.child(self.uuid!).child("MEMO_TIME").setValue(self.travelInfo.MEMO_TIME)
+      self.ref.child(self.uuid!).child("trafficinfo").setValue(self.travelInfo.trafficinfo)
+          self.ref.child(self.uuid!).child("nowday").setValue(self.nowday)
+          self.ref.child(self.uuid!).child("usedNum").setValue( self.usedNum)
        
     }
-    
+    //顯示推薦資料
     func  showTravelInfo()
     {
         self.performSegue(withIdentifier: "ShowTravelInfo", sender: self)
     }
+    //重新推薦
     func resetTravalInfo()
     {
         repeat
@@ -175,13 +169,12 @@ class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
         
         self.performSegue(withIdentifier: "ShowTravelInfo", sender: self)
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
+    //傳值使用自訂過場動畫
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowTravelInfo"
         {
@@ -191,18 +184,17 @@ class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
             destination.travelinfo = travelInfo
         }
     }
-    
     //動畫實體
     func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         return customPresentAnimationController
     }
-
+    //使用alamofire呼叫api
     func alamofireStar(SetTravalInfo settravalInfo:Bool)
     {
         Alamofire.request(self.urlString, headers: nil).responseJSON(completionHandler: {response in
             if let sSON = response.result.value
             {
-                //JSON資料處理
+                //swiftyJSON資料處理
                 let swiftyJsonVar = JSON(sSON)
                 
                  self.dataArrayTaipei = swiftyJsonVar["result"]["results"].arrayValue
@@ -216,29 +208,15 @@ class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
                     self.randomNum  = arc4random_uniform(UInt32(self.dataArrayTaipei.count - 1))
                 }while self.usedNum.contains(self.randomNum)
                 
-                
                 self.usedNum.append(self.randomNum)
                 
                 self.imgsArr = self.dataArrayTaipei[Int(self.randomNum)]["file"].stringValue.components(separatedBy: "http://")
-                
-                //print(dataArrayTaipei[0]["stitle"].stringValue)
-                
-                
-                //print(swiftyJsonVar["result"]["results"][0]["MRT"])
-                //for anyobject in swiftyJsonVar["result"]["results"].array!
-                //{
-                //self.anu = anyobject as AnyObject!
-                //print(anyobject["MRT"])
-                // self.dataArray = anyobject.arrayObject as! [AnyObject]
-                //}
                 
                 if(settravalInfo){
                 self.setTravalInfo(Json: self.dataArrayTaipei)
                 
                 self.updateFireBase()
                 }
-
-                
                 //資料完成後  出現推薦button
                 let loginRegisterButton:UIButton =
                     {
